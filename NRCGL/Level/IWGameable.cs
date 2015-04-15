b9e3795d09f9a20e113fb88c1f56dae4e6fc32ef
nrc34 <nuno.ramalho.costa@gmail.com>
2015-04-15 23:08:28 +0100

    start level management

---
+++ NRCGL/Level/IWGameable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_NRCGL.NRCGL.Level
+{
+    /// <summary>
+    /// Define the necessary methods to be used at the GameWindow. 
+    /// </summary>
+    interface IWGameable
+    {
+        void Load();
+        void Update();
+        void Render();
+        void CheckKeyBoard();
+    }
+}
---
+++ NRCGL/Level/Level.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_NRCGL.NRCGL.Level
+{
+    abstract class Level : IWGameable
+    {
+
+        public void Load()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Render()
+        {
+            
+        }
+
+        public void CheckKeyBoard()
+        {
+            
+        }
+
+
+    }
+}
