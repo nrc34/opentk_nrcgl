e883f84caad61cf03b596f85b74eb44a9e5eff2e
nrc34 <nuno.ramalho.costa@gmail.com>
2015-05-09 23:43:01 +0100

    MyGameMenu started

---
+++ Game/MyGameMenu.cs
@@ -0,0 +1,89 @@
+﻿using OpenTK;
+using OpenTK_NRCGL.NRCGL.Level;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_NRCGL.Game
+{
+    class MyGameMenu : GameLevel
+    {
+        public MyGameMenu(GameWindow gameWindow, 
+                          int id = 0, 
+                          string name = "menu") 
+            : base(id, name, gameWindow)
+        {
+            GameWindow = gameWindow;
+            ID = id;
+            Name = name;
+
+        }
+
+        public override void Load()
+        {
+            base.Load();
+        }
+
+        public override void LoadAudio()
+        {
+            base.LoadAudio();
+        }
+
+        public override void LoadShapes()
+        {
+            base.LoadShapes();
+        }
+
+        public override void LoadTextures()
+        {
+            base.LoadTextures();
+        }
+
+        public override void LoadShadowMap()
+        {
+            base.LoadShadowMap();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Run()
+        {
+            base.Run();
+        }
+
+        public override void CheckKeyBoard()
+        {
+            base.CheckKeyBoard();
+        }
+
+        public override void CheckMouse()
+        {
+            base.CheckMouse();
+        }
+
+        public override void Finish()
+        {
+            base.Finish();
+        }
+
+        public override void Unload()
+        {
+            base.Unload();
+        }
+
+        public override void Render()
+        {
+            base.Render();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
